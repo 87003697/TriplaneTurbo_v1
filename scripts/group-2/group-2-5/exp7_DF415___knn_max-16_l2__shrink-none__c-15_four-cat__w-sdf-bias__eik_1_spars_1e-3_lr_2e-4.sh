3a672aa381662154359a9f6d7062967ca045f0a5
87003697 <870036797@qq.com>
2025-04-22 22:42:12 +0800

    update

---
+++ scripts/group-2/group-2-5/exp7_DF415___knn_max-16_l2__shrink-none__c-15_four-cat__w-sdf-bias__eik_1_spars_1e-3_lr_2e-4.sh
@@ -0,0 +1,4 @@
+CUDA_VISIBLE_DEVICES=1,2,3,4,5,6,7 python launch.py \
+    --config configs/primiturbo_trd_group-2/primiturbo_trd_group-2-5/DF415___knn_max-16_l2__shrink-none__c-15_four-cat__w-sdf-bias__eik_1_spars_1e-3_lr_2e-4.yaml \
+    --train \
+  data.prompt_library="dreamfusion_415_prompt_library"
