93aeca41c9472b6627b7d6745254b0ab5af99ba9
87003697 <870036797@qq.com>
2025-04-22 11:06:21 +0800

    update

---
+++ scripts/group-2/group-2-4/exp2_DF415___trd-ddpm_shrink-p_c-15_four-add_lr_2e-4__ns-100-64__sd-5-v1_mv-20-v1_rd-20-v1__eik_1_spars_1e-3_p_0_eps_01.sh
@@ -0,0 +1,4 @@
+CUDA_VISIBLE_DEVICES=1,2,3,4,5,6,7 python launch.py \
+    --config configs/primiturbo_trd_group-2/primiturbo_trd_group-2-4/DF415___trd-ddpm_shrink-p_c-15_four-add_lr_2e-4__ns-100-64__sd-5-v1_mv-20-v1_rd-20-v1__eik_1_spars_1e-3_p_0_eps_01.yaml \
+    --train \
+  data.prompt_library="dreamfusion_415_prompt_library"
