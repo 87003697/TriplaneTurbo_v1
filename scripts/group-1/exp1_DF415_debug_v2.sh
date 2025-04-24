09110d152370d4c81d647f266687b3fc495e3130
87003697 <870036797@qq.com>
2025-04-24 18:41:58 +0800

    update

--- scripts/group-1/exp1_DF415_debug_v2.sh
+++ scripts/group-1/exp1_DF415_debug_v2.sh
@@ -11,12 +11,12 @@ export NCCL_P2P_DISABLE=1
 #     data.prompt_library="dreamfusion_415_prompt_library"
 
 
-CUDA_VISIBLE_DEVICES=1,2,3,4,5,6,7 python launch.py \
-    --config configs/primiturbo_trd_group-1/DF415_debug_v2.yaml  \
-    --train \
-  data.prompt_library="dreamfusion_415_prompt_library"
-  
-# CUDA_VISIBLE_DEVICES=1 python launch.py \
+# CUDA_VISIBLE_DEVICES=1,2,3,4,5,6,7 python launch.py \
 #     --config configs/primiturbo_trd_group-1/DF415_debug_v2.yaml  \
 #     --train \
-#     data.prompt_library="dreamfusion_415_prompt_library"
+#   data.prompt_library="dreamfusion_415_prompt_library"
+  
+CUDA_VISIBLE_DEVICES=0 python launch.py \
+    --config configs/primiturbo_trd_group-1/DF415_debug_v2.yaml  \
+    --train \
+    data.prompt_library="dreamfusion_415_prompt_library"
