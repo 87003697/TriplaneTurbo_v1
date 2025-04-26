f12827672f4990332052bcc2523e0454889f0595
87003697 <870036797@qq.com>
2025-04-26 10:46:45 +0800

    update

---
+++ scripts/group-3/group-3-1/exp12_DF415___attr-tex_knn_max-08_l1__shrink-none_no-bg_gs-v3__c-15_four-add__basic-bias__eik_01_lr_2e-4.sh
@@ -0,0 +1,4 @@
+CUDA_VISIBLE_DEVICES=1,2,3,4,5,6,7 python launch.py \
+    --config configs/primiturbo_trd_group-3/primiturbo_trd_group-3-1/DF415___attr-tex_knn_max-08_l1__shrink-none_no-bg_gs-v3__c-15_four-add__basic-bias__eik_01_lr_2e-4.yaml \
+    --train \
+  data.prompt_library="dreamfusion_415_prompt_library"
