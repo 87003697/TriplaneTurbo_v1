dc9b58e4d4a80eb12b7c1216b059e1cc882d3ac7
87003697 <870036797@qq.com>
2025-04-28 15:26:02 +0800

    update

--- scripts/group-3/group-3-2/exp1_nerf_imp-128+64_no-bg__knn_1__TRD-PM___lr_2e-4.sh
+++
@@ -1,4 +0,0 @@
-CUDA_VISIBLE_DEVICES=1,2,3,4,5,6,7 python launch.py \
-    --config configs/primiturbo_trd_group-3/primiturbo_trd_group-3-2/nerf_imp-128+64_no-bg__knn_1__TRD-PM___lr_2e-4.yaml \
-    --train \
-  data.prompt_library="dreamfusion_415_prompt_library"
