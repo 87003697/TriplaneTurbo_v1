92f338bf668d9e6a2a7a748be0780b28641c405a
Zhiyuan_Ma <zm2354@columbia.edu>
2025-04-29 11:43:21 +0800

    update

---
+++ scripts/group-3/group-3-3/exp6_neus-100-prj__depth_32-0.1+32-0.5_no-bg__knn_4__TRD-PM___lr_2e-4.sh
@@ -0,0 +1,4 @@
+CUDA_VISIBLE_DEVICES=1,2,3,4,5,6,7 python launch.py \
+    --config configs/primiturbo_trd_group-3/primiturbo_trd_group-3-3/neus-100-prj__depth_32-0.1+32-0.5_no-bg__knn_4__TRD-PM___lr_2e-4.yaml \
+    --train \
+  data.prompt_library="dreamfusion_415_prompt_library"
