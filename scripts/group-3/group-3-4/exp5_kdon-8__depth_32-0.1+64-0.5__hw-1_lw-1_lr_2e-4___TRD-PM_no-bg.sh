428324cf4b527e6958a54b92ad410a751f582aa5
87003697 <870036797@qq.com>
2025-04-29 19:39:02 +0800

    update

--- scripts/group-3/group-3-4/exp5_kdon-8__depth_32-0.1+64-0.5__hw-1_lw-1_lr_2e-4___TRD-PM_no-bg.sh
+++
@@ -1,4 +0,0 @@
-CUDA_VISIBLE_DEVICES=1,2,3,4,5,6,7 python launch.py \
-    --config configs/primiturbo_trd_group-3/primiturbo_trd_group-3-4/kdon-8__depth_32-0.1+64-0.5__hw-1_lw-1_lr_2e-4___TRD-PM_no-bg.yaml \
-    --train \
-  data.prompt_library="dreamfusion_415_prompt_library"
