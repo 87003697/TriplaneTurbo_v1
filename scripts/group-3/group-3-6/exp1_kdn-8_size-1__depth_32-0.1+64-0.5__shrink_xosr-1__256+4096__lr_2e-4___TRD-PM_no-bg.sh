3bf4cc12ebfabda19ee1631e41a949752591b20d
87003697 <870036797@qq.com>
2025-04-30 14:32:12 +0800

    support_mixed_renderers

---
+++ scripts/group-3/group-3-6/exp1_kdn-8_size-1__depth_32-0.1+64-0.5__shrink_xosr-1__256+4096__lr_2e-4___TRD-PM_no-bg.sh
@@ -0,0 +1,4 @@
+CUDA_VISIBLE_DEVICES=1,2,3,4,5,6,7 python launch.py \
+    --config configs/primiturbo_trd_group-3/primiturbo_trd_group-3-6/kdn-8_size-1__depth_32-0.1+64-0.5__shrink_xosr-1__256+4096__lr_2e-4___TRD-PM_no-bg.yaml \
+    --train \
+  data.prompt_library="dreamfusion_415_prompt_library"
