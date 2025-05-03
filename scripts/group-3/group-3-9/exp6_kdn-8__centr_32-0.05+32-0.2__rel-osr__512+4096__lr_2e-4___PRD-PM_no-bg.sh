3590b6cf0ee8088f2195d63aee41353c144e0d69
87003697 <870036797@qq.com>
2025-05-03 13:16:53 +0800

    update

---
+++ scripts/group-3/group-3-9/exp6_kdn-8__centr_32-0.05+32-0.2__rel-osr__512+4096__lr_2e-4___PRD-PM_no-bg.sh
@@ -0,0 +1,4 @@
+CUDA_VISIBLE_DEVICES=1,2,3,4,5,6,7 python launch.py \
+    --config configs/primiturbo_trd_group-3/primiturbo_trd_group-3-9/kdn-8__centr_32-0.05+32-0.2__rel-osr__512+4096__lr_2e-4___PRD-PM_no-bg.yaml \
+    --train \
+  data.prompt_library="dreamfusion_415_prompt_library"
