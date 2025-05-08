281bd42f89d85360fc703da6a66edb02c381bf00
Zhiyuan_Ma <zm2354@columbia.edu>
2025-05-08 22:21:22 +0800

    update_gather_wo_opacity

---
+++ scripts/group-4/group-4-1/exp1_kdn-4__nerf_opa-dpt_32-0.1+32-0.5__rel-none__512+4096__lr_2e-4___TRD-PM_no-bg.sh
@@ -0,0 +1,4 @@
+CUDA_VISIBLE_DEVICES=1,2,3,4,5,6,7 python launch.py \
+    --config configs/primiturbo_trd_group-4/primiturbo_trd_group-4-1/kdn-4__nerf_opa-dpt_32-0.1+32-0.5__rel-none__512+4096__lr_2e-4___TRD-PM_no-bg.yaml  \
+    --train \
+  data.prompt_library="dreamfusion_415_prompt_library"
