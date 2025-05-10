742dccd23a25718426f8e0df28b71a3bde2fc782
Zhiyuan_Ma <zm2354@columbia.edu>
2025-05-10 20:33:01 +0800

    update

---
+++ scripts/group-4/group-4-3/exp14_kdn-4__volsdf-30_sdf-mah_opa-dpt_32-0.2+32-0.5__mul-lvl_c_-0.1__512+4096__lr_1e-4___TRD-IM_no-bg.sh
@@ -0,0 +1,4 @@
+CUDA_VISIBLE_DEVICES=1,2,3,4,5,6,7 python launch.py \
+    --config configs/primiturbo_trd_group-4/primiturbo_trd_group-4-3/kdn-4__volsdf-30_sdf-mah_opa-dpt_32-0.2+32-0.5__mul-lvl_c_-0.1__512+4096__lr_1e-4___TRD-IM_no-bg.yaml \
+    --train \
+  data.prompt_library="dreamfusion_415_prompt_library"
