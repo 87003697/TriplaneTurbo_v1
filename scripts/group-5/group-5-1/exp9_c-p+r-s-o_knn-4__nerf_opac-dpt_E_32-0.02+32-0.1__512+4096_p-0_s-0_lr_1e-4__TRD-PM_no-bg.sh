b16748ebe9fb1f17bce80e7ca9bd76a00ba21a51
Zhiyuan_Ma <zm2354@columbia.edu>
2025-05-14 19:32:08 +0800

    exps_group-5

---
+++ scripts/group-5/group-5-1/exp9_c-p+r-s-o_knn-4__nerf_opac-dpt_E_32-0.02+32-0.1__512+4096_p-0_s-0_lr_1e-4__TRD-PM_no-bg.sh
@@ -0,0 +1,4 @@
+CUDA_VISIBLE_DEVICES=1,2,3,4,5,6,7 python launch.py \
+    --config configs/primiturbo_trd_group-5/primiturbo_trd_group-5-1/c-p+r-s-o_knn-4__nerf_opac-dpt_E_32-0.02+32-0.1__512+4096_p-0_s-0_lr_1e-4__TRD-PM_no-bg.yaml \
+    --train \
+  data.prompt_library="dreamfusion_415_prompt_library"
