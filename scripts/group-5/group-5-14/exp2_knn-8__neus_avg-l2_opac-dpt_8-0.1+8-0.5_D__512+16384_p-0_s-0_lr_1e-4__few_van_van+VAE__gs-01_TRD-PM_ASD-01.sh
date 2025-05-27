27d8947b87d028175728bacb08b48814dc3681c8
Zhiyuan_Ma <zm2354@columbia.edu>
2025-05-27 11:04:00 +0800

    update

---
+++ scripts/group-5/group-5-14/exp2_knn-8__neus_avg-l2_opac-dpt_8-0.1+8-0.5_D__512+16384_p-0_s-0_lr_1e-4__few_van_van+VAE__gs-01_TRD-PM_ASD-01.sh
@@ -0,0 +1,4 @@
+CUDA_VISIBLE_DEVICES=1,2,3,4,5,6,7 python launch.py \
+    --config configs/primiturbo_trd_group-5/primiturbo_trd_group-5-14/knn-8__neus_avg-l2_opac-dpt_8-0.1+8-0.5_D__512+16384_p-0_s-0_lr_1e-4__few_van_van+VAE__gs-01_TRD-PM_ASD-01.yaml \
+    --train \
+  data.prompt_library="dreamfusion_415_prompt_library"
